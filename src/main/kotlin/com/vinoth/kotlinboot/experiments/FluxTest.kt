a5d10395ed4e9b4f2604284fe7723b914c158030
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-01-03 11:43:36 +0530

    Test driving Spring Reactor

---
+++ src/main/kotlin/com/vinoth/kotlinboot/experiments/FluxTest.kt
@@ -0,0 +1,34 @@
+package com.vinoth.kotlinboot.experiments
+
+
+import reactor.core.publisher.Flux
+import reactor.core.publisher.toFlux
+
+fun main(args: Array<String>) {
+
+
+    val fluxInt = (1..8).toFlux()
+
+    fluxInt
+            .filter { it < 5 }
+            .subscribe { println(it) }
+
+    println("-- map--")
+
+
+    fluxInt
+            .map { it * it }
+            .subscribe { println(it) }
+
+    println("---- Flat Map ---")
+
+    val nameFlux = arrayListOf<Flux<String>>(
+            arrayListOf("Mark", "Steve").toFlux(),
+            arrayListOf("Dev").toFlux(),
+            arrayListOf("Lead").toFlux()).toFlux()
+
+    nameFlux.
+            flatMap { it }
+            .subscribe { println(it.toUpperCase())}
+
+}
